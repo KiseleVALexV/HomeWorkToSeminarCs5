7a97179276d5733b36a39f4d0431abd30c0eb36e
Alexey <manoil77@list.ru>
2022-11-15 07:50:12 +0300

    Add Zadacha034

---
+++ Zadacha034_ChetnyeChislaVMassive/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] numbers = new int[4];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(100,1000);
+
+    Console.Write(numbers[i] + " ");
+}
+
+int summa = 0;
+for (int i=0; i < numbers.Length; i++)
+    if (numbers[i] % 2 == 0 )
+    {
+        summa++;
+    }
+Console.WriteLine("\n" + summa);
+Console.ReadKey();
