c9ea25cd95c9f3eb8c28223a4e937fa3973752de
Alexey <manoil77@list.ru>
2022-11-15 07:50:43 +0300

    Add Zadacha036

---
+++ Zadacha036_SummaElementovNechetnyhPozici/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] numbers = new int[4];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-20,20);
+
+    Console.Write(numbers[i] + " ");
+}
+int summa = 0;
+for (int i=1; i < numbers.Length; i+=2)
+    summa += numbers[i];
+Console.WriteLine("\n" + summa);
+Console.ReadKey();
